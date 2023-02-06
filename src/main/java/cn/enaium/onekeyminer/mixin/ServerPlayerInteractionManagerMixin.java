db6479923e2f0ee1a0ffc5cfa50a5afa5ab28f22
Enaium <wyy-666@hotmail.com>
2023-02-06 11:16:56 +0800

    1.3.1

--- src/main/java/cn/enaium/onekeyminer/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/cn/enaium/onekeyminer/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,11 +1,11 @@
-/**
+/*
  * Copyright 2022 Enaium
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,12 +18,11 @@ package cn.enaium.onekeyminer.mixin;
 import cn.enaium.onekeyminer.OneKeyMiner;
 import cn.enaium.onekeyminer.model.Config;
 import cn.enaium.onekeyminer.util.BlockUtil;
-import net.minecraft.block.BlockState;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.*;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -37,17 +36,18 @@ import java.util.List;
 @Mixin(ServerPlayerInteractionManager.class)
 public abstract class ServerPlayerInteractionManagerMixin {
     @Shadow
+    public ServerPlayerEntity player;
+    @Shadow
+    public ServerWorld world;
+
+    @Shadow
     public abstract boolean tryBreakBlock(BlockPos pos);
 
-    @Shadow public ServerPlayerEntity player;
-    private final List<BlockPos> searched = new ArrayList<>();
-
-    @Inject(at = @At(value = "HEAD"), method = "method_21717")
-    private void finishMining(BlockPos pos, PlayerActionC2SPacket.Action action, CallbackInfo ci) {
-
+    @Inject(at = @At(value = "HEAD"), method = "finishMining")
+    private void finishMining(BlockPos pos, PlayerActionC2SPacket.Action action, String reason, CallbackInfo ci) {
         ItemStack stack = player.inventory.getInvStack(player.inventory.selectedSlot);
         if (stack != null) {
-            boolean canMine = stack.getItem().canMine(BlockUtil.getBlockState(pos), MinecraftClient.getInstance().world, pos, player);
+            boolean canMine = stack.getItem().canMine(world.getBlockState(pos), world, pos, player);
             if (canMine && (stack.getItem() instanceof MiningToolItem || stack.getItem() instanceof ShearsItem) && player.isSneaking()) {
                 Config config = OneKeyMiner.config;
                 List<String> list = new ArrayList<>();
@@ -62,29 +62,33 @@ public abstract class ServerPlayerInteractionManagerMixin {
                 } else if (stack.getItem() instanceof ShearsItem) {
                     list.addAll(config.shears);
                 }
-
-                searched.clear();
-                breakBreakBlock(pos, config.limit, list);
+                final String name = BlockUtil.getName(world, pos);
+                if (list.contains(name)) {
+                    breakBreakBlock(name, pos, pos, config.limit, new ArrayList<>());
+                }
             }
         }
     }
 
 
-    private void breakBreakBlock(BlockPos blockPos, int limit, List<String> blocks) {
+    private void breakBreakBlock(String name, BlockPos target, BlockPos blockPos, int limit, List<BlockPos> searched) {
         int radius = 1;
         for (int x = -radius; x <= radius; x++) {
             for (int y = -radius; y <= radius; y++) {
                 for (int z = -radius; z <= radius; z++) {
                     BlockPos newBlockPos = new BlockPos(blockPos.getX() + x, blockPos.getY() + y, blockPos.getZ() + z);
-                    BlockState blockState = BlockUtil.getBlockState(newBlockPos);
-                    String name = BlockUtil.getName(blockState.getBlock().getDropTableId());
+                    final double cbrt = Math.ceil(Math.cbrt(limit));
+                    if (name.equals(BlockUtil.getName(world, newBlockPos))
+                            && !searched.contains(newBlockPos)
+                            && searched.size() < (cbrt * cbrt * cbrt)) {
 
-                    if (blocks.contains(name) && !searched.contains(newBlockPos) && searched.size() < limit) {
+                        if (OneKeyMiner.config.pickaxe.contains(name) && newBlockPos.getManhattanDistance(target) > cbrt) {
+                            break;
+                        }
+
                         searched.add(newBlockPos);
-
-
                         tryBreakBlock(newBlockPos);
-                        breakBreakBlock(newBlockPos, limit, blocks);
+                        breakBreakBlock(name, target, newBlockPos, limit, searched);
                     }
                 }
             }

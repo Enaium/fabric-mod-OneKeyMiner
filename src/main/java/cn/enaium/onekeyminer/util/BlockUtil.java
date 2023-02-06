62dde847d5a26d80f47227421e6970c27f4243a3
Enaium <wyy-666@hotmail.com>
2023-02-06 09:53:14 +0800

    1.3.1

--- src/main/java/cn/enaium/onekeyminer/util/BlockUtil.java
+++ src/main/java/cn/enaium/onekeyminer/util/BlockUtil.java
@@ -1,11 +1,11 @@
-/**
+/*
  * Copyright 2022 Enaium
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,10 +15,9 @@
  */
 package cn.enaium.onekeyminer.util;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 /**
  * @author Enaium
@@ -28,7 +27,7 @@ public class BlockUtil {
         return identifier.getNamespace() + ":" + identifier.getPath().substring(identifier.getPath().lastIndexOf("/") + 1);
     }
 
-    public static BlockState getBlockState(BlockPos blockPos) {
-        return MinecraftClient.getInstance().world.getBlockState(blockPos);
+    public static String getName(World world, BlockPos blockPos) {
+        return getName(world.getBlockState(blockPos).getBlock().getLootTableId());
     }
 }
